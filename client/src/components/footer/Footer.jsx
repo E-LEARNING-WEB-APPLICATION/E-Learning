996ee86188e7d5216a242a8a5cf7c948c8fc8db1
Swarajsr1234 <swarajrawate28@gmail.com>
2025-10-12 00:34:01 +0530

    Implimented Footer for Application

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='bg-dark text-light py-3 mt-5'>
+            <div className='container text-center'>
+                <p className='mb-1'>
+                    &copy;2025 E-Learning Platform. All rights reserved.
+                </p>
+                <p className='mb-0'>
+                    Designed with ❤️ by Our Team
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer

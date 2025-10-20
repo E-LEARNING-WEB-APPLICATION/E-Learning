3507cb9f57035f93250b86bcfc3e9b1ba01ac87d
Shivraj-Somavanshi <shivasomavanshi@gmail.com>
2025-10-20 11:15:09 +0530

    created all routes in the app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css'
 import StudentLayout from './components/student/layout/StudentLayout';
 import StudentDashboard from './pages/student/dashboard/Dashboard';
@@ -9,12 +9,19 @@ import WishList from './pages/student/wishlist/WishList';
 import InstructorLayout from './components/instructor/layout/InstructorLayout';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import AddCourse from './pages/instructor/AddCourse/AddCourse';
+import AddedCourses from './pages/instructor/AddedCourses/AddedCourses';
+import AddSection from './pages/instructor/AddSection/AddSection';
+import ShowSection from './pages/instructor/ShowSections/ShowSection';
+import ShowTopic from './pages/instructor/ShowTopics/ShowTopic';
+import AddTopic from './pages/instructor/AddTopics/AddTopic';
 
 function App() {
 
   return (
     <>
       <div>
+      
         <Routes>
           <Route path='/' element={<Navigate to="/login" />} />
           <Route path='/login' element={<Login />} />
@@ -27,12 +34,44 @@ function App() {
         </Routes>
         <Routes>
           <Route path='/instructor' element={<InstructorLayout />}>
-            <Route path='dashboard' element={<InstructorDashboard />} />
+            <Route path='dashboard' element={<InstructorDashboard />}>  </Route>
+
+          <Route path='addCourse' element={<AddCourse />}></Route>
+          
+          <Route path='addedCourses' element={<AddedCourses/>}> </Route>
+          <Route path='addedCourses/add-section' element={<AddSection/>}></Route>
+          <Route path='addedCourses/show-sections' element={<ShowSection/>}> </Route>
+          
+        
+          <Route path='addedCourses/show-sections/add-topic' element={<AddTopic/>}></Route>
+          <Route path='addedCourses/show-sections/show-topics' element={<ShowTopic/>}></Route>
           </Route>
+          
+          
         </Routes>
+        
       </div>
     </>
   );
 }
 
 export default App
+
+
+
+      
+
+      
+      
+
+      
+
+      
+
+      
+      
+
+
+
+ 
+     

e151f13bead359c8486cd6e598b5d553c296344d
Omkar Gandhi <gandhioms16@gmail.com>
2025-11-10 16:08:03 +0530

    refactor(admin-courses): replaced local array with mock data

--- client/src/components/admin/courses/TableView.jsx
+++ client/src/components/admin/courses/TableView.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const TableView = ({courses}) => {
+const TableView = ({ courses }) => {
   return (
     <div>
-        <table className='table'>
-            <thead>
-                <tr>
-                    <td>course id</td>
-                    <td>name</td>
-                    <td>category</td>
-                    <td>instructor</td>
-                    <td>price</td>
-                </tr>
-            </thead>
-            <tbody>
-                {courses.map((course)=>{
-                    return <tr key={course.course_id}>
-                        <td>{course.course_id}</td>
-                        <td>{course.course_name}</td>
-                        <td>{course.category}</td>
-                        <td>{course.instructor_name}</td>
-                        <td>{course.fees}</td>
-                    </tr>
-                })}
-            </tbody>
-        </table>
+      <table className="table">
+        <thead>
+          <tr>
+            <td>course id</td>
+            <td>name</td>
+            <td>category</td>
+            <td>instructor</td>
+            <td>price</td>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => {
+            return (
+              <tr key={course.courseId}>
+                <td>{course.courseId}</td>
+                <td>{course.course_name}</td>
+                <td>{course.category}</td>
+                <td>{course.iId}</td>
+                <td>{course.fees}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default TableView
+export default TableView;

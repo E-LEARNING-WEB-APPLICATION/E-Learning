478641842a2d81803285cb002c05af67c633c2b0
Sanket Sudhakar Raut <111580160+Sanketr113@users.noreply.github.com>
2025-12-07 18:20:43 +0530

    fix(StudentLayout): clean up formatting and remove unnecessary code

--- client/src/components/student/layout/StudentLayout.jsx
+++ client/src/components/student/layout/StudentLayout.jsx
@@ -4,28 +4,14 @@ import Footer from "../../footer/Footer";
 import StudentNavbar from "../navbar/StudentNavbar";
 
 const StudentLayout = () => {
-<<<<<<< HEAD
     return (
         <div>
             <StudentNavbar />
-            <main className='container-fluid mt-0'>
+            <main className="container-fluid mt-0">
                 <Outlet />
             </main>
             <Footer />
         </div>
-    )
-}
-=======
-  return (
-    <div>
-      <StudentNavbar />
-      <main className="container-fluid mt-0 p-0">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+    );
 };
->>>>>>> b221a4485fee0590874f6684ad0bbe75e6cebca4
-
 export default StudentLayout;

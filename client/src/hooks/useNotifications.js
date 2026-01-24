8dbf88260ed5b18eca8ae06cec6ea4ee39f41103
Omkar Gandhi <gandhioms16@gmail.com>
2026-01-24 17:34:07 +0530

    feat: adds useNotifications hook

---
+++ client/src/hooks/useNotifications.js
@@ -0,0 +1,10 @@
+import { fetchNotifications } from "@/services/admin/notification";
+import { useQuery } from "@tanstack/react-query";
+
+export function useNotifications(isRead) {
+  return useQuery({
+    queryKey: ["notifications", { isRead }],
+    queryFn: () => fetchNotifications(isRead),
+    initialData: [],
+  });
+}

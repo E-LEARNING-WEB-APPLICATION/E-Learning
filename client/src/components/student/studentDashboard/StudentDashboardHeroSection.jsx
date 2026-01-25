b0c83730fa06fb26fee6afa3c62b7a22cac40998
Sanket Sudhakar Raut <111580160+Sanketr113@users.noreply.github.com>
2026-01-26 01:51:20 +0530

    feat(student-dashboard): implement course status handling and update enrollment link functionality

--- client/src/components/student/studentDashboard/StudentDashboardHeroSection.jsx
+++ client/src/components/student/studentDashboard/StudentDashboardHeroSection.jsx
@@ -1,8 +1,33 @@
+import { getCourseStatusById } from "@/services/courseService";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const StudentDashboardHeroSection = ({ courses }) => {
     const discounted = courses.filter((c) => c.discount > 0);
     const heroCourses = discounted.length > 0 ? discounted : courses;
+    const navigate = useNavigate();
+    const handleView = async (courseId) => {
+        try {
+            const response = await getCourseStatusById(courseId);
+            console.log(response);
+            if (!response.success) {
+                navigate("/student/course-details", {
+                    state: {
+                        courseId: courseId,
+                    },
+                });
+            } else {
+                navigate("/student/enrolled-course-details", {
+                    state: {
+                        courseId: courseId,
+                    },
+                });
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
     return (
         <section
             className="py-5 text-white"
@@ -100,7 +125,7 @@ const StudentDashboardHeroSection = ({ courses }) => {
                                                     )}
                                                 </div>
                                                 <a
-                                                    href="#"
+                                                    onClick={() => { handleView(course.id) }}
                                                     className="btn btn-warning btn-sm mt-3 fw-semibold">
                                                     Enroll Now
                                                 </a>

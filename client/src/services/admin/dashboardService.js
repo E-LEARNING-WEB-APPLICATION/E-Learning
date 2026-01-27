c0736a0c700e87d80298e60d8bac3696eb96d6cb
Omkar Gandhi <gandhioms16@gmail.com>
2026-01-27 20:46:28 +0530

    feat: updated admin dashboard to use apis

--- client/src/services/admin/dashboardService.js
+++ client/src/services/admin/dashboardService.js
@@ -36,14 +36,12 @@ export const fetchTotalStudentsActiveInLast = (duration) => {
 };
 
 export const fetchTotalRevenue = () => {
-  return apiRequest(() =>
-    apiClient.get(`${ADMIN_BASE_PATH}/statistics/revenue`),
-  );
+  return apiRequest(() => apiClient.get(`${API_BASE_PATH}/statistics/revenue`));
 };
 
 export const fetchRevenueTrendByMonth = (duration) => {
   return apiRequest(() =>
-    apiClient.get(`${ADMIN_BASE_PATH}/student/revenue/by-month`, {
+    apiClient.get(`${API_BASE_PATH}/statistics/revenue/by-month`, {
       params: {
         duration, // string "3m", "6m", "1y"
       },
@@ -53,7 +51,7 @@ export const fetchRevenueTrendByMonth = (duration) => {
 
 export const fetchStudentEnrolledTrendByMonth = (duration) => {
   return apiRequest(() =>
-    apiClient.get(`${ADMIN_BASE_PATH}/student/revenue/by-month`, {
+    apiClient.get(`${ADMIN_BASE_PATH}/student/enrolled/monthly`, {
       params: {
         duration, // number default 6
       },
@@ -71,6 +69,30 @@ export const fetchInstructorsLeaderboard = (params) => {
   return apiRequest(() =>
     apiClient.get(`${ADMIN_BASE_PATH}/student/revenue/by-month`, {
       params,
+    }),
+  );
+};
+
+export const fetchCategoryDistribution = (params) => {
+  return apiRequest(() =>
+    apiClient.get(`${API_BASE_PATH}/statistics/category/distribution`, {
+      params,
+    }),
+  );
+};
+
+export const fetchTopCourseRatingOverview = (params) => {
+  return apiRequest(() =>
+    apiClient.get(`${API_BASE_PATH}/statistics/course/by-rating`, {
+      params,
+    }),
+  );
+};
+
+export const fetchTopCoursesByEnrollments = (params) => {
+  return apiRequest(() =>
+    apiClient.get(`${API_BASE_PATH}/statistics/course/enrollments`, {
+      params, // top: number
     }),
   );
 };
